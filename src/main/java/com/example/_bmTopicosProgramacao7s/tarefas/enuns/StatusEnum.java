766ee87bc4670869c8eb1c2c25e1fa8aea0d588d
WillianKakihata <ra-22015763-2@alunos.unicesumar.edu.br>
2025-05-22 02:38:13 -0300

    :sparkles: feat(init TarefasModels and StatusEnum): building the status models, and the statusEnum enums

--- src/main/java/com/example/_bmTopicosProgramacao7s/tarefas/enuns/StatusEnum.java
+++ src/main/java/com/example/_bmTopicosProgramacao7s/tarefas/enuns/StatusEnum.java
@@ -1,4 +1,16 @@
 package com.example._bmTopicosProgramacao7s.tarefas.enuns;
 
 public enum StatusEnum {
+    EM_PROCESSO("andamento"),
+    INICIO("inicio"),
+    FINALIZADO("finalizado");
+
+    String status;
+    StatusEnum(String andamento) {
+        this.status = andamento;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
--- src/main/java/com/example/_bmTopicosProgramacao7s/tarefas/model/Tarefas.java
+++ src/main/java/com/example/_bmTopicosProgramacao7s/tarefas/model/Tarefas.java
@@ -1,4 +1,29 @@
 package com.example._bmTopicosProgramacao7s.tarefas.model;
 
+import com.example._bmTopicosProgramacao7s.tarefas.enuns.StatusEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "tarefas")
+@Entity(name = "tarefas")
 public class Tarefas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "titulos")
+    String titulo;
+
+    @Column(name = "descricao")
+    String descricao;
+
+    @Column(name = "status")
+    StatusEnum status;
+
 }
