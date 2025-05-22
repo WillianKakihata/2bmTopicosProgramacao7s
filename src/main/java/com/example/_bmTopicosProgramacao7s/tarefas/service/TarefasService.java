be119ade4d77daefd2406ef1615bc33523a52373
WillianKakihata <ra-22015763-2@alunos.unicesumar.edu.br>
2025-05-22 02:47:06 -0300

    :sparkles: feat(fix TarefasRepository, and create function in TarefasService): fix a repository for tasks, and, creating a create function in TarefasService

--- src/main/java/com/example/_bmTopicosProgramacao7s/tarefas/service/TarefasService.java
+++ src/main/java/com/example/_bmTopicosProgramacao7s/tarefas/service/TarefasService.java
@@ -1,4 +1,18 @@
 package com.example._bmTopicosProgramacao7s.tarefas.service;
 
+
+import com.example._bmTopicosProgramacao7s.tarefas.model.Tarefas;
+import com.example._bmTopicosProgramacao7s.tarefas.repository.TarefasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TarefasService {
+    @Autowired
+    TarefasRepository tarefasRepository;
+
+    public Tarefas create(Tarefas tarefas){
+        Tarefas novaTarefa = this.tarefasRepository.save(tarefas);
+        return novaTarefa;
+    }
 }
